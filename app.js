788ee00f17bd331a2b92a59af4aeac6deea71986
Héla Ben Khalfallah <helabenkhalfallah@MacBook-Pro.local>
2021-06-20 11:50:27 +0200

    [Feature] node js project template setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Start server
+const portNumber = '8080';
+app.listen(portNumber, () => {
+  console.debug('server started - ' + portNumber);
+});
+
+module.exports = app;
